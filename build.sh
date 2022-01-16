4adc6b3850aa03e65ce8f3a1cb823e04fb260faf
Arun007coder <deepalali1007.agali@gmail.com>
2022-01-16 16:10:42 +0530

    * Added htop clfs-bootscripts dropbear Iana-Etc netplug and zlib to extra packages * Added patches to makefile * updated packages.txt * Added networking

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ export OUT="$PWD"/out
 export ROOT="$PWD"/root
 export MNT="$PWD"/mnt
 export EPKG="$PWD"/extra-packages
+export EXTRA="$PWD"/ext
+export AUTO
 
 export PKGL="$PWD"/packages.txt
 
@@ -36,33 +38,39 @@ GLIBC_VERSION=2.34
 
 # Warning: changing this will break the build system
 
-export HOST=$(echo ${MACHTYPE} | sed "s/-[^-]*/-cross/")
-export TARGET=x86_64-unknown-linux-gnu
-export CPU=k8
-export ARCH=x86_64
+HOST=$(echo ${MACHTYPE} | sed "s/-[^-]*/-cross/")
+TARGET=x86_64-unknown-linux-gnu
+CPU=k8
+ARCH=x86_64
+
+export HOST TARGET CPU ARCH
 
 # END Warning
 
 echo "Warning: This script can only build linux for the current machine's architecture"
-echo "Press enter to continue or ctrl+c to exit"
-read -r
+if [ $AUTO -eq 1 ]; then
+    echo ""
+    sleep 10
+else
+    echo "Press enter to continue or ctrl+c to exit"
+    echo ""
+    read -r
+fi
 
-case $1 in
-    -s)
-        echo "Welcome to the MINLINUX build system"
-        echo "This script will build a minimal linux"
-        echo "Please be patient, it may take a while"
-        ;;
-    *)
-        echo "Welcome to the MINLINUX build system"
-        echo "This script will build a minimal linux"
-        echo "Please be patient, it may take a while"
-        echo "Press enter to continue"
-        read -r
-        ;;
-esac
+if [ $AUTO -eq 1 ]; then
+    echo "Welcome to the MINLINUX build system"
+    echo "This script will build a minimal linux"
+    echo "Please be patient, it may take a while"
+    sleep 10
+else
+    echo "Welcome to the MINLINUX build system"
+    echo "This script will build a minimal linux"
+    echo "Please be patient, it may take a while"
+    echo "press enter to continue or ctrl+c to exit"
+    read -r
+fi
 
-mkdir -p "$SRC" "$INITRD" "$LOG" "$OUT" "$INS" "$ROOT" "$MNT" "$EPKG"
+mkdir -p "$SRC" "$INITRD" "$LOG" "$OUT" "$INS" "$ROOT" "$MNT" "$EPKG" "$INS"/patch
 mkdir -p "$INITRD"/{bin,dev,etc,lib,lib64,mnt,home,proc,root,run,sbin,sys,tmp,usr,var,srv,opt}
 mkdir -p "$INITRD"/usr/{bin,include,lib,lib64,sbin,src,share,src,opt}
 
@@ -103,7 +111,17 @@ if ls "$INS"/LINUX_KERNEL 1> /dev/null 2>&1; then
 else
     echo "Installing Linux source"
     cd "$SRC"/linux-"$LINUX_VERSION" || exit
+    if [ $AUTO -eq 1 ]; then
     make defconfig
+    else
+    echo "Do you want to use the default configuration? (y/n)"
+    read -r
+    if [ "$REPLY" = "y" ]; then
+        make defconfig
+    else
+        make menuconfig
+    fi
+    fi
     make -j8 || echo "error building linux kernel" && exit 1 | tee /"$LOG"/linux.log
     touch "$INS"/LINUX_KERNEL
     cp -rv "$INITRD"/usr/include/* "$INITRD"/usr/include/linux
@@ -132,16 +150,12 @@ if ls "$INS"/GLIBC 1> /dev/null 2>&1; then
     echo "GLIBC already compiled"
 else
     cd glibc-"$GLIBC_VERSION" || exit
-    echo $PWD
     mkdir -pv glibc-build
     cd glibc-build || exit
-    echo $PWD
     echo "libc_cv_forced_unwind=yes" > config.cache
     echo "libc_cv_c_cleanup=yes" >> config.cache
     echo "libc_cv_ssp=no" >> config.cache
     echo "libc_cv_ssp_strong=no" >> config.cache
-    echo $LD_LIBRARY_PATH
-    #sleep 10
     ../configure \
     --host=${TARGET} --build=${HOST} \
     --disable-profile --enable-add-ons --with-tls \
@@ -153,7 +167,6 @@ else
     make V=1 install_root=${INITRD}/ install | tee ${LOG}/GLIBC.log || exit 2
     touch "$INS"/GLIBC
     echo "GLIBC installed"
-    #sleep 10
     cd ../..
 fi
 
@@ -179,7 +192,17 @@ if ls "$INS"/BUSYBOX 1> /dev/null 2>&1; then
     echo "Busybox source already compiled"
 else
     echo "Compiling Busybox"
+    if [ $AUTO -eq 1 ]; then
     make defconfig
+    else
+    echo "Do you want to use the default configuration? (y/n)"
+    read -r
+    if [ "$REPLY" = "y" ]; then
+        make defconfig
+    else
+        make menuconfig
+    fi
+    fi
     #sed 's/^.*CONFIG_STATIC[^_].*$/CONFIG_STATIC=y/' -i .config
     make -j8 && \
     make CONFIG_PREFIX="$INITRD" install > /"$LOG"/busybox.log
@@ -200,7 +223,7 @@ cd "$INITRD" || exit
 
 rm etc/passwd
 cat > etc/passwd << "EOF"
-root::0:0:root:/root:/bin/sh
+root:x:0:0:root:/root:/bin/bash
 EOF
 
 rm etc/group
@@ -223,7 +246,7 @@ cat > etc/fstab << "EOF"
 # file system  mount-point  type   options          dump  fsck
 #                                                         order
 
-rootfs          /               auto    defaults        1      1
+rootfs          /               $AUTO    defaults        1      1
 proc            /proc           proc    defaults        0      0
 sysfs           /sys            sysfs   defaults        0      0
 devpts          /dev/pts        devpts  gid=4,mode=620  0      0
@@ -269,31 +292,10 @@ EOF
 rm etc/issue
 echo "$OS_NAME $OS_VERSION" > etc/issue
 
-rm etc/hostname
-echo TESTLinux > etc/hostname
+rm etc/HOSTNAME
+echo TESTLinux > etc/HOSTNAME
 
-mkdir etc/rc.d/
-rm etc/rc.d/startup
-cat > etc/rc.d/startup << "EOF"
-#!/bin/sh
-PATH=/sbin:/bin:/usr/sbin:/usr/bin #Initialize the environment variable PATH, the operating system executes the program by default to find the program in the directory specified by PATH
-
-runlevel=S #Set the system to single user mode
-prevlevel=N #Set the system to single user mode
-
-umask 022 #Specify the default permissions of the current user when creating files 
-
-export PATH runlevel prevlevel #Export environment variables
-
-mount -a #mount The file system specified in the fstab file
-
-#From/etc/sysconfig/Read the host name i
-
-mount -o rw,remount / #Remount the file system specified in the fstab file
-
-ldconfig # to start the dynamic linker
-
-EOF
+mkdir -p etc/rc.d/
 
 ln -svf ${INITRD}/proc/mounts ${INITRD}/etc/mtab
 
@@ -301,33 +303,6 @@ mkdir -pv var/{run,log}
 
 touch var/run/utmp var/log/{btmp,lastlog,wtmp}
 chmod -v 664 var/run/utmp var/log/lastlog
-
-rm init
-:<< "COM"
-echo '#!/bin/sh' >> init
-echo '' >> init
-echo 'mount -t proc proc /proc' >> init
-echo 'mount -t sysfs sysfs /sys' >> init
-echo 'mount -t devtmpfs udev /dev' >> init
-echo '' >> init
-echo 'sysctl -w kernel.printk="2 4 1 7"' >> init
-echo 'sysctl -w net.ipv4.ip_forward=1' >> init
-echo '' >> init
-echo 'wait 5' >> init
-echo '' >> init
-echo 'ldconfig' >> init
-echo 'mknod -m 622 /dev/console c 5 1' >> init
-echo 'mknod -m 666 /dev/null c 1 3' >> init
-echo 'mknod -m 666 /dev/zero c 1 5' >> init
-echo 'mknod -m 666 /dev/ptmx c 5 2' >> init
-echo 'mknod -m 666 /dev/tty c 5 0' >> init
-echo 'mknod -m 444 /dev/random c 1 8' >> init
-echo 'mknod -m 444 /dev/urandom c 1 9' >> init
-echo 'chown root:tty /dev/{console,ptmx,tty}' >> init
-echo '' >> init
-echo '/bin/sh' >> init
-echo 'poweroff -f' >> init
-COM
 
 mkdir -p etc/selinux/
 
@@ -366,6 +341,70 @@ cat > etc/ld.so.conf.d/default.conf << "EOF"
 /usr/lib64
 EOF
 
+rm etc/mdev.conf
+cat > etc/mdev.conf<< "EOF"
+# Devices:
+# Syntax: %s %d:%d %s
+# devices user:group mode
+
+# null does already exist; therefore ownership has to
+# be changed with command
+null    root:root 0666  @chmod 666 $MDEV
+zero    root:root 0666
+grsec   root:root 0660
+full    root:root 0666
+
+random  root:root 0666
+urandom root:root 0444
+hwrandom root:root 0660
+
+# console does already exist; therefore ownership has to
+# be changed with command
+console root:tty 0600 @mkdir -pm 755 fd && cd fd && for x in 0 1 2 3 ; do ln -sf /proc/self/fd/$x $x; done
+
+kmem    root:root 0640
+mem     root:root 0640
+port    root:root 0640
+ptmx    root:tty 0666
+
+# ram.*
+ram([0-9]*)     root:disk 0660 >rd/%1
+loop([0-9]+)    root:disk 0660 >loop/%1
+sd[a-z].*       root:disk 0660
+hd[a-z][0-9]*   root:disk 0660
+
+tty             root:tty 0666
+tty[0-9]        root:root 0600
+tty[0-9][0-9]   root:tty 0660
+ttyO[0-9]*      root:tty 0660
+pty.*           root:tty 0660
+vcs[0-9]*       root:tty 0660
+vcsa[0-9]*      root:tty 0660
+
+ttyLTM[0-9]     root:dialout 0660 @ln -sf $MDEV modem
+ttySHSF[0-9]    root:dialout 0660 @ln -sf $MDEV modem
+slamr           root:dialout 0660 @ln -sf $MDEV slamr0
+slusb           root:dialout 0660 @ln -sf $MDEV slusb0
+fuse            root:root  0666
+
+# misc stuff
+agpgart         root:root 0660  >misc/
+psaux           root:root 0660  >misc/
+rtc             root:root 0664  >misc/
+
+# input stuff
+event[0-9]+     root:root 0640 =input/
+ts[0-9]         root:root 0600 =input/
+
+# v4l stuff
+vbi[0-9]        root:video 0660 >v4l/
+video[0-9]      root:video 0660 >v4l/
+
+# load drivers for usb devices
+usbdev[0-9].[0-9]       root:root 0660 */lib/mdev/usbdev
+usbdev[0-9].[0-9]_.*    root:root 0660
+EOF
+
 cat > etc/ld.so.conf.d/x86_64-linux-gnu.conf << "EOF"
 # /etc/ld.so.conf.d/x86_64-linux-gnu.conf
 
@@ -374,46 +413,101 @@ cat > etc/ld.so.conf.d/x86_64-linux-gnu.conf << "EOF"
 /usr/local/lib/x86_64-linux-gnu
 EOF
 
-rm etc/addusr
-cat > etc/addusr << "EOF"
+# Networking
+
+rm etc/network/interfaces
+cat > etc/network/interfaces << "EOF"
+auto eth0
+iface eth0 inet dhcp
+EOF
+
+rm etc/network.conf
+cat > etc/network.conf << "EOF"
+# /etc/network.conf
+# Global Networking Configuration
+# interface configuration is in /etc/network.d/
+
+INTERFACE="eth0"
+
+# set to yes to enable networking
+NETWORKING=yes
+
+# set to yes to set default route to gateway
+USE_GATEWAY=no
+
+# set to gateway IP address
+GATEWAY=10.0.2.2
+EOF
+
+mkdir -pv etc/network/if-{post-{up,down},pre-{up,down},up,down}.d
+mkdir -pv usr/share/udhcpc
+
+rm usr/share/udhcpc/default.script
+cat > usr/share/udhcpc/default.script << "EOF"
 #!/bin/sh
+# udhcpc Interface Configuration
+# Based on http://lists.debian.org/debian-boot/2002/11/msg00500.html
+# udhcpc script edited by Tim Riker <Tim@Rikers.org>
 
-username=$1
+[ -z "$1" ] && echo "Error: should be called from udhcpc" && exit 1
 
-adduser adduser "$username" --shell /bin/bash --home /home/"$username"
-passwd "$username"
+RESOLV_CONF="/etc/resolv.conf"
+[ -n "$broadcast" ] && BROADCAST="broadcast $broadcast"
+[ -n "$subnet" ] && NETMASK="netmask $subnet"
 
-echo new user "$username" added
-sleep 10
+case "$1" in
+    deconfig)
+            /sbin/ifconfig $interface 0.0.0.0
+            ;;
+
+    renew|bound)
+            /sbin/ifconfig $interface $ip $BROADCAST $NETMASK
+
+            if [ -n "$router" ] ; then
+                    while route del default gw 0.0.0.0 dev $interface ; do
+                            true
+                    done
+
+                    for i in $router ; do
+                            route add default gw $i dev $interface
+                    done
+            fi
+
+            echo -n > $RESOLV_CONF
+            [ -n "$domain" ] && echo search $domain >> $RESOLV_CONF
+            for i in $dns ; do
+                    echo nameserver $i >> $RESOLV_CONF
+            done
+            ;;
+esac
+
+exit 0
 EOF
-sudo chmod +x etc/addusr
 
-:<< "COM"
-rm etc/fstab
-cat > etc/fstab << "EOF"
-# file system  mount-point  type   options          dump  fsck
-#                                                         order
+chmod +x usr/share/udhcpc/default.script
 
-# rootfs          /               auto    defaults        1      1
-proc            /proc           proc    defaults        0      0
-sysfs           /sys            sysfs   defaults        0      0
-devpts          /dev/pts        devpts  gid=4,mode=620  0      0
-tmpfs           /dev/shm        tmpfs   defaults        0      0
+sudo chroot $INITRD /bin/sh -c "ldconfig"
+if [ $AUTO -eq 1 ]; then
+    echo
+    sudo chroot $INITRD passwd << "EOF"
+root
+root
+
 EOF
-COM
+else
+    echo "Do you want to make a new root password?"
+    echo -n "(y/n) "
+    read -r ANSWER
 
-echo "Do you want to add new user? (y/n)"
-read answer
-if [ "$answer" == "y" ]; then
-    echo "Enter username:"
-    read username
-    mkdir -p home/"$username"
-    sudo chroot "$INITRD" /usr/bin/env \
-    HOME=/root \
-    PS1='(lfs chroot) \u:\w\$ ' \
-    PATH="/bin:/usr/bin:/sbin:/usr/sbin" \
-    LD_LIBRARY_PATH="/lib:/usr/lib:/lib64:/usr/lib64" \
-    /bin/bash --login +h -c "/etc/addusr $username"
+    if [ "$ANSWER" = "y" ]; then
+        sudo chroot $INITRD passwd
+    else
+        sudo chroot $INITRD passwd << "EOF"
+root
+root
+
+EOF
+    fi
 fi
 
 FILES="$(ls ${INITRD}/usr/lib64/*.a)"
@@ -421,11 +515,33 @@ for file in $FILES; do
     rm -f $file
 done
 
-find ${INITRD}/{,usr/}{bin,lib,sbin} -type f -exec sudo strip --strip-debug '{}' ';' 2>&1
-find ${INITRD}/{,usr/}lib64 -type f -exec sudo strip --strip-debug '{}' ';' 2>&1
+find ${INITRD}/{,usr/}{bin,lib,sbin} -type f -exec sudo strip --strip-debug '{}' ';' > /dev/null 2>&1
+find ${INITRD}/{,usr/}lib64 -type f -exec sudo strip --strip-debug '{}' ';' > /dev/null 2>&1
 
 sudo chmod -R 777 .
 cp "$SRC"/linux-"$LINUX_VERSION"/arch/x86/boot/bzImage "$OUT"/bzImage-"$LINUX_VERSION"-generic
+
+if [ $AUTO -eq 1 ]; then
+    mkdir -p "$ROOT"/boot/grub
+    rm "$ROOT"/boot/grub/grub.cfg
+    cat > "$ROOT"/boot/grub/grub.cfg << "EOF"
+set default=0
+set timeout=5
+EOF
+    printf "menuentry '%s_CDROM' {\n" $OS_NAME >> "$ROOT"/boot/grub/grub.cfg
+    printf "set root=(cd)\n" >> "$ROOT"/boot/grub/grub.cfg
+    printf "linux /boot/bzImage-%s-generic root=/dev/sr0 ro" $LINUX_VERSION >> "$ROOT"/boot/grub/grub.cfg
+    printf "\n}\n\n" >> "$ROOT"/boot/grub/grub.cfg
+    printf "menuentry '%s_HD' {\n" $OS_NAME >> "$ROOT"/boot/grub/grub.cfg
+    printf "set root=(hd0,msdos1)\n" >> "$ROOT"/boot/grub/grub.cfg
+    printf "linux /boot/bzImage-%s-generic root=/dev/sda1 ro" $LINUX_VERSION >> "$ROOT"/boot/grub/grub.cfg
+    printf "\n}" >> "$ROOT"/boot/grub/grub.cfg
+    cp "$OUT"/bzImage-"$LINUX_VERSION"-generic "$ROOT"/boot/bzImage-"$LINUX_VERSION"-generic
+    cp -r -u "$INITRD"/* "$ROOT"/
+    "$GRUB_DIR"/grub-mkrescue --compress=xz -o "$OUT"/"$OS_NAME"-"$OS_VERSION".iso "$ROOT"
+    rm -rf "$ROOT"
+    exit
+fi
 
 while true; do
 
@@ -461,7 +577,6 @@ EOF
         printf "menuentry 'LINUX' {\n" >> "$ROOT"/boot/grub/grub.cfg
         printf "linux /boot/bzImage-$LINUX_VERSION-generic root=/dev/sda1 ro" >> "$ROOT"/boot/grub/grub.cfg
         printf "\n}" >> "$ROOT"/boot/grub/grub.cfg
-        cp "$OUT"/initrd.img "$ROOT"/boot/initrd.img
         cp "$OUT"/bzImage-"$LINUX_VERSION"-generic "$ROOT"/boot/bzImage-"$LINUX_VERSION"-generic
         cp -r -u "$INITRD"/* "$ROOT"/
         if ls "$device"1 > /dev/null 2>&1; then
@@ -506,7 +621,7 @@ EOF
         cp "$OUT"/initrd.img "$ROOT"/boot/initrd.img
         cp "$OUT"/bzImage-"$LINUX_VERSION"-generic "$ROOT"/boot/bzImage-"$LINUX_VERSION"-generic
         cp -r -u "$INITRD"/* "$ROOT"/
-        "$GRUB_DIR"/grub-mkrescue -o "$OUT"/"$OS_NAME"-"$OS_VERSION".iso "$ROOT"
+        "$GRUB_DIR"/grub-mkrescue --compress=xz -o "$OUT"/"$OS_NAME"-"$OS_VERSION".iso "$ROOT"
 
         rm -rf "$ROOT"
         ;;
